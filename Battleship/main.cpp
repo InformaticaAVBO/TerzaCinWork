5ca01ee5bbfe1f93ddd1be07efcbe99bf7f2523c
SabrineFekih <sabrinefekih284@gmail.com>
2026-01-08 07:56:14 +0000

    Ho inserito il progetto "Battleship"

---
+++ Battleship/main.cpp
@@ -0,0 +1,13 @@
+/*
+    main.cpp
+    Sabrine Fekih - 08/01/2026
+    Battleship Game
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl;
+	return 0;
+}
