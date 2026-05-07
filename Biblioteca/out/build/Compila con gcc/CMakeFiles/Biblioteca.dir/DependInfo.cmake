425095df81c3a26ff0b761c406e8c54830601589
Sandro Gallo <5992750+sandrogallo@users.noreply.github.com>
2026-05-07 19:27:52 +0000

    cmake

---
+++ Biblioteca/out/build/Compila con gcc/CMakeFiles/Biblioteca.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/TerzaCinWork/Biblioteca/Biblioteca.cpp" "CMakeFiles/Biblioteca.dir/Biblioteca.cpp.o" "gcc" "CMakeFiles/Biblioteca.dir/Biblioteca.cpp.o.d"
+  "/workspaces/TerzaCinWork/Biblioteca/Libro.cpp" "CMakeFiles/Biblioteca.dir/Libro.cpp.o" "gcc" "CMakeFiles/Biblioteca.dir/Libro.cpp.o.d"
+  "/workspaces/TerzaCinWork/Biblioteca/main.cpp" "CMakeFiles/Biblioteca.dir/main.cpp.o" "gcc" "CMakeFiles/Biblioteca.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Biblioteca/out/build/Compila con gcc/CMakeFiles/Biblioteca.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "Biblioteca"
+  "Biblioteca.pdb"
+  "CMakeFiles/Biblioteca.dir/Biblioteca.cpp.o"
+  "CMakeFiles/Biblioteca.dir/Biblioteca.cpp.o.d"
+  "CMakeFiles/Biblioteca.dir/Libro.cpp.o"
+  "CMakeFiles/Biblioteca.dir/Libro.cpp.o.d"
+  "CMakeFiles/Biblioteca.dir/main.cpp.o"
+  "CMakeFiles/Biblioteca.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Biblioteca.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
